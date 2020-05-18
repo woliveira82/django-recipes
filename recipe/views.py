98b3491e53a3f05372aa53dbf97bdaabacbd4e55
Weiner Oliveira <weiner.oliveira@smartnx.com>
2020-05-17 22:33:05 -0300

    Update template

--- recipe/views.py
+++ recipe/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    recipe_list = [
+        {'recipe_name':'Recipe 01'},
+        {'recipe_name':'Recipe 02'},
+        {'recipe_name':'Recipe 03'},
+    ]
+    data = {
+        'best_recipe': recipe_list
+    }
+    return render(request, 'index.html', data)
 
 
 def recipe(request):
