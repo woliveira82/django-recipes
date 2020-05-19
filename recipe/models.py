9dc27002590d000d70868e5cd516b25e3bb4ef69
Weiner Oliveira <weiner.oliveira@smartnx.com>
2020-05-18 22:21:35 -0300

    Create migration. Update database conection.

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    prepare_time = models.IntegerField()
+    serves = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
