5960979943b12030c47b632b111ba3c6e64f7434
woliveira82 <woliveira82@gmail.com>
2020-05-19 00:30:35 -0300

    Update admin

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Recipe
 
-admin.site.register(Recipe)
+class RecipeList(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category')
+    list_display_links = ('id', 'name')
+
+admin.site.register(Recipe, RecipeList)
